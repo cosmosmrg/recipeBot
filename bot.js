e7ecab383fe794eb9a900a24fa8bc3128a60ef89
Sawarin Sodsangsook <may@Sawarins-MacBook-Pro.local>
2017-07-13 16:28:18 +0700

    add bot typing and pause

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ var botBuilder = require('claudia-bot-builder'),
     eTitle = entity => ((entity.title || '') + ' by ' + (entity.publisher || '')),
     format = text => (text && text.substring(0, 80));
 var Promise = require('bluebird');
-
+const botTyping = new fbTemplate.ChatAction('typing_on'),
+      botPause = new fbTemplate.Pause(1000);
 module.exports = botBuilder((request,apiReq) => {
   apiReq.lambdaContext.callbackWaitsForEmptyEventLoop = false;
   var res;
@@ -14,7 +15,8 @@ module.exports = botBuilder((request,apiReq) => {
     res = api.findEntities(keyword)
           .then((entities) => {
             if (entities.count<1) {
-              return `Unfortunately, could not find anything about ${keyword}`;
+              return [ botTyping.get(),
+                botPause.get(),`Unfortunately, could not find anything about ${keyword}`];
             }
             else{
               const generic = new fbTemplate.generic();
@@ -23,54 +25,33 @@ module.exports = botBuilder((request,apiReq) => {
                     .addButton('View Ingredient', "GET"+entity.recipe_id)
                     .addImage(entity.image_url);
               });
-              return generic.get();
+              return [ botTyping.get(),
+                botPause.get(),generic.get()];
             }
           })
           .catch(function (e) {
-            return "Something happens "+keyword+"\n" +e.message;
+            return [ botTyping.get(),
+              botPause.get(),"Something happens "+keyword+"\n" +e.message]
           });
   }
   else if (request.text.startsWith("GET")){
     const id = request.text.substring(3)
     res = api.entityClaims(id)
-          .then((claims) => {
-              let title = 'Ingredient of ' + eTitle(claims.recipe);
-              return title + ':\n' + claims.recipe.ingredients.join('\n');
-          })
-          .catch(function (e) {
-              return "Something happens "+"Getting result is"+"\n" +e.message;
-          });
+            .then((claims) => {
+                let title = 'Ingredient of ' + eTitle(claims.recipe);
+                return [ botTyping.get(),
+                  botPause.get(),title + ':\n' + claims.recipe.ingredients.join('\n')];
+            })
+            .catch(function (e) {
+                return [ botTyping.get(),
+                  botPause.get(),"Something happens "+"Getting result is"+"\n" +e.message];
+            })
 
   }
   else{
-    res = "Try typing find {keyword}"
+    res = [ botTyping.get(),
+      botPause.get(),
+      'to find recipe\n type \'find {keyword} \' ']
   }
-  return res;
+  return res
 });
-
-// api.findEntities(request.text)
-//   .then((entities) => {
-//     if (entities.count<1) {
-//     return `Unfortunately, could not find anything about ${request.text}`;
-//     }
-//     else if (entities.count === 1) {
-//       let title = 'Ingredient of ' + eTitle(entities.recipes[0]);
-//       return api.entityClaims(entities.recipes[0].recipe_id)
-//             .then((claims) => {
-//                 return title + ':\n' + claims.recipe.ingredients.join('\n');
-//               }
-//             );
-//     }
-//     else {
-//       const generic = new fbTemplate.generic();
-//       entities.recipes.slice(0, 9).forEach(entity => {
-//         generic.addBubble(format(entity.title), format(entity.publisher+"\n Ranking :"+entity.social_rank))
-//             .addButton('View Ingredient', "GET"+entity.title)
-//             .addImage(entity.image_url);
-//       });
-//       return generic.get();
-//     }
-//   })
-//   .catch(function (e) {
-//     return "Something happens "+request.text+"\n" +e.message;
-//   });
