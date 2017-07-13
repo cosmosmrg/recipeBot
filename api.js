16266b49832e346e42cd269cf4662ba70df2c78d
cosmosmrg <calopc.canova@gmail.com>
2017-07-14 01:43:12 +0700

    update to lastest

--- api.js
+++ api.js
@@ -3,21 +3,24 @@ var Promise = require('bluebird');
 module.exports = {
   findEntities(recipe) {
     return new Promise((resolve, reject)=>{
-      unirest.get("https://community-food2fork.p.mashape.com/search?key=7aa7ef8b34dc4f50624c413ed7f4ba89&q="+recipe)
+      unirest.get(
+        "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?"
+        +"instructionsRequired=true&limitLicense=false&number=10&offset=0&query="+recipe)
       .header("X-Mashape-Key", "AhiIivJDcDmshNjoOCs0RchW8vWvp1EU6D0jsnoCH8UhKAud8r")
       .header("Accept", "application/json")
       .end(function (result) {
-        resolve( JSON.parse(result.body))
+        resolve( result.body)
       })
     })
   },
   entityClaims (entityId) {
     return new Promise((resolve, reject)=>{
-      unirest.get("https://community-food2fork.p.mashape.com/get?key=7aa7ef8b34dc4f50624c413ed7f4ba89&rId="+entityId)
+      unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+      +"/recipes/informationBulk?ids="+entityId+"&includeNutrition=false")
       .header("X-Mashape-Key", "AhiIivJDcDmshNjoOCs0RchW8vWvp1EU6D0jsnoCH8UhKAud8r")
       .header("Accept", "application/json")
       .end(function (result) {
-        resolve(JSON.parse(result.body));
+        resolve(result.body);
       })
     })
   }
--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ var botBuilder = require('claudia-bot-builder'),
     fbTemplate = botBuilder.fbTemplate,
     excuse = require('huh'),
     api = require('./api'),
-    eTitle = entity => ((entity.title || '') + ' by ' + (entity.publisher || '')),
     format = text => (text && text.substring(0, 80));
 var Promise = require('bluebird');
 const botTyping = new fbTemplate.ChatAction('typing_on'),
@@ -14,16 +13,21 @@ module.exports = botBuilder((request,apiReq) => {
     const keyword = request.text.substring(4)
     res = api.findEntities(keyword)
           .then((entities) => {
-            if (entities.count<1) {
+            if (entities.number<1) {
               return [ botTyping.get(),
                 botPause.get(),`Unfortunately, could not find anything about ${keyword}`];
             }
             else{
               const generic = new fbTemplate.generic();
-              entities.recipes.slice(0, 9).forEach(entity => {
-                generic.addBubble(format(entity.title), format(entity.publisher+"\n Ranking :"+entity.social_rank))
-                    .addButton('View Ingredient', "GET"+entity.recipe_id)
-                    .addImage(entity.image_url);
+              const baseurl = "https://spoonacular.com/recipeImages/";
+              entities.results.forEach(entity => {
+                generic.addBubble(format(entity.title), format("Prepare time : "+entity.readyInMinutes))
+                    .addButton('View Recipe', "GET"+entity.id)
+                    .addImage(entity.image?
+                      baseurl+encodeURI(entity.image)
+                    :
+                      null
+                    );
               });
               return [ botTyping.get(),
                 botPause.get(),generic.get()];
@@ -31,27 +35,115 @@ module.exports = botBuilder((request,apiReq) => {
           })
           .catch(function (e) {
             return [ botTyping.get(),
-              botPause.get(),"Something happens "+keyword+"\n" +e.message]
+              botPause.get(),"Something happens in searching"+keyword+":\n" +e.message]
           });
   }
   else if (request.text.startsWith("GET")){
     const id = request.text.substring(3)
     res = api.entityClaims(id)
+            .then((res)=> res[0])
             .then((claims) => {
-                let title = 'Ingredient of ' + eTitle(claims.recipe);
-                return [ botTyping.get(),
-                  botPause.get(),title + ':\n' + claims.recipe.ingredients.join('\n')];
+                var final = [ botTyping.get(),botPause.get()]
+                const title = new fbTemplate.Generic()
+                      .addBubble(format(claims.title + " by "+claims.creditText),format("Prepare time :"+claims.readyInMinutes
+                      +"|"+"Serving :"+claims.servings+"|"+"Like :"+claims.aggregateLikes))
+                     .addImage(claims.image?
+                       encodeURI(claims.image)
+                     :
+                     null
+                      )
+                     .get()
+                final.push(title);
+                const options = new fbTemplate.Text('What\'s you want to do next?')
+                                .addQuickReply('Ingredients', 'ingre-'+id)
+                                .addQuickReply('Instructions', 'inst-'+id)
+                                .addQuickReply('New Recipe', '{keyword}')
+                                .get();
+                final.push(options);
+                return final;
             })
             .catch(function (e) {
                 return [ botTyping.get(),
-                  botPause.get(),"Something happens "+"Getting result is"+"\n" +e.message];
+                  botPause.get(),"Something happens in Getting result:"+"\n" +e.message];
             })
 
+  }
+  else if(request.text.startsWith("inst-")){
+    const id = request.text.substring(5);
+    res = api.entityClaims(id)
+            .then((res)=> res[0])
+            .then((claims) => {
+                var final = [ botTyping.get(),botPause.get()]
+                final.push(new fbTemplate.text("Instructions :").get())
+                if(claims.analyzedInstructions){
+                  var arr =Array.apply(null, Array(Math.floor(claims.analyzedInstructions[0].steps.length/4)+1))
+                  .map(function (_, i) {return i;});
+                  arr.forEach((i)=>{
+                    claims.analyzedInstructions[0].steps.slice(i*4,i*4+3).forEach((instruction) =>{
+                      final.push(new fbTemplate.text("Step :"+(instruction.number)+"\n"+instruction.step)
+                      .get());
+                    })
+
+                  })
+                }else{
+                  final.push(new fbTemplate.text(claims.instructions).get())
+                }
+                const options = new fbTemplate.Text('What\'s you want to do next?')
+                                .addQuickReply('Ingredients', 'ingre-'+id)
+                                .addQuickReply('Instructions', 'inst-'+id)
+                                .addQuickReply('New Recipe', '{keyword}')
+                                .get();
+                final.push(options);
+
+                return final;
+            })
+            .catch(function (e) {
+                return [ botTyping.get(),
+                  botPause.get(),"Something happens in Getting ingredient:"+"\n" +e.message];
+            })
+  }
+  else if(request.text.startsWith("ingre-")){
+    const id = request.text.substring(6);
+    res = api.entityClaims(id)
+            .then((res)=> res[0])
+            .then((claims) => {
+                var final = [ botTyping.get(),botPause.get()]
+
+                final.push(new fbTemplate.text("Ingredients :").get())
+                var arr =Array.apply(null, Array(Math.floor(claims.extendedIngredients.length/4)))
+                .map(function (_, i) {return i;});
+                arr.forEach((i)=>{
+                  const ingredients = new fbTemplate.List("compact");
+                  claims.extendedIngredients.slice(i*4,i*4+3).forEach((ingredient) =>{
+                    ingredients.addBubble(format(ingredient.name + " Amount :"
+                        +ingredient.amount + " "+ingredient.unitShort),
+                        format(ingredient.originalString))
+                        .addImage(ingredient.image?
+                          encodeURI(ingredient.image)
+                        :
+                        null
+                        )
+                  })
+                  final.push(ingredients.get())
+                })
+                const options = new fbTemplate.Text('What\'s you want to do next?')
+                                .addQuickReply('Ingredients', 'ingre-'+id)
+                                .addQuickReply('Instructions', 'inst-'+id)
+                                .addQuickReply('New Recipe', '{keyword}')
+                                .get();
+                final.push(options);
+
+                return final;
+            })
+            .catch(function (e) {
+                return [ botTyping.get(),
+                  botPause.get(),"Something happens in Getting instructions :"+"\n" +e.message];
+            })
   }
   else{
     res = [ botTyping.get(),
       botPause.get(),
-      'to find recipe\n type \'find {keyword} \' ']
+      'To find recipe type \'find '+ request.text +'\' ']
   }
   return res
 });
